2acabff08fe30be3a3a309a7d3fafbc35eeb32a1
chldltkrz <chldltkrz@gmail.com>
2024-11-25 16:04:39 +0900

    added Item info page

--- lib/pages/main_page/main_page.dart
+++ lib/pages/main_page/main_page.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:silvermarket/pages/item_regi_page/item_regi_page.dart';
+import 'package:silvermarket/pages/item_info_page/item_info_page.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: ItemRegiPage());
+    return Scaffold(body: ItemInfoPage());
   }
 }
